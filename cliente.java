4db787438d86c976db4056e4f30c1c06f7576e55
elwese <cesar.ews20@gmail.com>
2024-11-07 00:00:55 -0500

    termino

---
+++ cliente.java
@@ -0,0 +1,32 @@
+
+
+public class cliente{
+    public boolean generarInforme(String formato,String medioNoti){
+        if (formato.equals("Word")) {
+            
+        }
+        else if (formato.equals("Excel")) {
+            //
+        }
+        else if (formato.equals("PDF")){
+
+        }
+        
+        if (medioNoti.equals("Email")) {
+            
+        }
+        else if (true) {
+            //
+        }
+        else{}
+        return true;
+    }
+
+    public boolean generarInformePersonalizado(String formato, String color, String estilo, String fuente, String medioNoti){
+
+        return true;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
